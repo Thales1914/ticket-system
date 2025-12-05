6e35496d97ec91a9ef7f3ed3b93ff78895f9f18a
Thales <thalesbarbosa.barbosa10@gmail.com>
2025-12-05 09:48:45 -0300

    retirada do sistema de mensagens

--- src/main/java/com/seuprojeto/tickets/dto/CreateMessageDTO.java
+++
@@ -1,9 +0,0 @@
-package com.seuprojeto.tickets.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record CreateMessageDTO(
-        @NotNull Long ticketId,
-        @NotBlank String message
-) {}
--- src/main/java/com/seuprojeto/tickets/repository/TicketMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.seuprojeto.tickets.repository;
-
-import com.seuprojeto.tickets.entity.TicketMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketMessageRepository extends JpaRepository<TicketMessage, Long> {
-}
